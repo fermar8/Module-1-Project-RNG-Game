754b5cfe3e86ae9d37cea0e1dbcf3154033426a4
fermar8 <ferran.ms8@gmail.com>
2020-11-01 19:58:37 +0100

    Add initial validator and database

---
+++ scripts/Validator.js
@@ -0,0 +1,81 @@
+"use strict"
+
+class Validator {
+    constructor(){
+        this.invalidEmailError = 'Enter a valid email address';
+        this.emailExistsError = 'The entered email addres is already taken';
+        this.passwordError = 'Password must be at least 6 characters long';
+        this.repeatPasswordError = 'Password and repeat password must match!'
+
+        this.errors = {
+            invalidEmailError: this.invalidEmailError,
+            passwordError: this.passwordError,
+            repeatPasswordError: this.repeatPasswordError
+        }
+    }
+
+    validateValidEmail = (email) => {
+
+        if(this.emailSyntaxIsvalid(email)) {
+
+            delete this.errors.invalidEmailError;
+
+        } else {
+
+            this.errors.invalidEmailError = this.invalidEmailError;
+        }
+
+    };
+
+    emailSyntaxValid = (email) => {
+
+        const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+        const emailIsValid = emailRegEx.test(email);
+        return emailIsValid;
+
+    }
+
+    validateUniqueEmail = (newEmail) => {
+        const users = db.getAllUsers();
+
+        let emailUnique = true;
+
+        users.forEach((userObj) =>{
+            if(userObj.email === newEmail) {
+                emailUnique = false;
+            }
+        });
+
+            if(emailUnique) {
+                delete this.errors.emailExistsError;
+            } else {
+                this.errors.emailExistsError = this.emailExistsError;
+            }
+    };
+
+    validatePassword = (password) => {
+        if(password.lenght >=6) {
+            delete this.errors.passwordError;
+        } else {
+            this.errors.passwordError = this.passwordError;
+        }
+    };
+    
+    validateRepeatPassword = (password, repeatPassword) => {
+        if(password === repeatPassword) {
+            delete this.errors.repeatPasswordError;
+        } else {
+            this.errors.repeatPasswordError = this.repeatPasswordError;
+        }
+    };
+
+
+    getErrors = () => {
+        return this.errors;
+    };
+
+}
+
+const validator = new Validator(); 
+
