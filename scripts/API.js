e01d990188c23721b585858c12a73745e1e242ff
fermar8 <ferran.ms8@gmail.com>
2020-11-02 11:27:17 +0100

    Add API.js first version. Returns undefined

---
+++ scripts/API.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const gameList = document.querySelector('.games-list');
+
+
+fetch("https://rawg-video-games-database.p.rapidapi.com/games", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+        "x-rapidapi-key": "b8f3d8b159msh27d0065bced469cp15f8fejsn4e0630fb1464"
+    }
+})
+.then(response => {
+    console.log(response);
+    const pr = response.json();
+    return pr;
+})
+.then( (data) => {
+console.log('data', data);
+
+const article = document.createElement('article');
+article.innerHTML =`
+<img src="${data.results.background_image}" />
+      <h3>${data.results.name}</h3>
+`
+
+gameList.appendChild(article);
+
+
+})
+
