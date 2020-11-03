32fd8875a7068c3b6bd91721a9878c75bca5efa6
fermar8 <ferran.ms8@gmail.com>
2020-11-03 19:56:54 +0100

    Add MVP Homepage.

--- scripts/API.js
+++ scripts/API.js
@@ -2,46 +2,84 @@
 
 //loadDash() {
 
-fetch("https://rawg-video-games-database.p.rapidapi.com/games?page=2", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-        "x-rapidapi-key": "b8f3d8b159msh27d0065bced469cp15f8fejsn4e0630fb1464"
-    }
-})
-.then(response => {
-    console.log(response);
-    const pr = response.json();
-    return pr;
-})
 
+function getGames () {
 
-.then( (data) => {
-    const allData = data.results;
-    allData.forEach(games => {
+    fetch("https://rawg-video-games-database.p.rapidapi.com/games?page=2", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "b8f3d8b159msh27d0065bced469cp15f8fejsn4e0630fb1464"
+        }
+    })
+    .then(response => {
+        console.log(response);
+        const pr = response.json();
+        return pr;
+    })
+    .then( (data) => {
         const gameList = document.querySelector('.games-list');
-    const createDiv = document.createElement('div')
-    createDiv.setAttribute('class','card' )
-    createDiv.style.width='18rem';
-    createDiv.innerHTML= `
-    <img src="${games.background_image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${games.name}</h5>
-      <p class="card-text">Rating: ${games.rating}/5</p>
-    </div>
-  `
-  console.log(data)
-  gameList.appendChild(createDiv);
+        const allData = data.results;
+        allData.forEach(games => {
+        const createDiv = document.createElement('div')
+        createDiv.setAttribute('class','card' )
+        createDiv.style.width='18rem';
+        createDiv.innerHTML= `
+        <img src="${games.background_image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${games.name}</h5>
+          <p class="card-text">Rating: ${games.rating}/5</p>
+        </div>
+      `
+      console.log(data.results)
+      gameList.appendChild(createDiv);
+    })
+    .catch((error)=>{})
 })
+}
+getGames()
 
+function getInfo () {
+
+    fetch("https://rawg-video-games-database.p.rapidapi.com/games?page=2", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "b8f3d8b159msh27d0065bced469cp15f8fejsn4e0630fb1464"
+        }
+    })
+    .then(response => {
+        console.log(response);
+        const pr = response.json();
+        return pr;
+    })
+    .then( (data) => {
+        const gameList = document.querySelector('.games-list');
+        const allData = data.results;
+        allData.forEach(games => {
+        const createDiv = document.createElement('div')
+        createDiv.setAttribute('class','card' )
+        createDiv.style.width='18rem';
+        createDiv.innerHTML= `
+       
+        <div class="card-body">
+          <h5 class="card-title">${games.name}</h5>
+          <p class="card-text">Rating: ${games.rating}/5</p>
+        </div>
+      `
+      console.log(data.results)
+      gameList.appendChild(createDiv);
+    })
+    .catch((error)=>{})
 })
+}
+getInfo()
 
 
 //}
 
 //loadDash()
 
-//document.getElementById("random-btn").addEventListener("click", loadDash());
 
 
 //<a href="#" class="btn btn-primary">Go somewhere</a>
